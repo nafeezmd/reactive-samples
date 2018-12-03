5ff16a76af07ec12f569db0bfc8e2374bf59e284
Abdul Nafeez <abdulnafeez.mohammad@morningstar.com>
2018-12-02 21:06:01 -0600

    base-setup: add springboot dependencies

---
+++ README.md
@@ -0,0 +1,1 @@
+# This project contains code samples from the book _Learning Springboot 2.0 (second edition) by Greg L Turnquist_
---
+++ src/main/java/com/labs/reactivesamples/ReactiveSamplesApplication.java
@@ -0,0 +1,12 @@
+package com.labs.reactivesamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveSamplesApplication.class, args);
+	}
+}
