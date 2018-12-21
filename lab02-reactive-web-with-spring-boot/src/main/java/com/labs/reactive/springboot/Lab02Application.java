1ff7bc6aabf2ec33eebc82d29d31584e34ae9eab
Abdul Nafeez <abdulnafeez.mohammad@morningstar.com>
2018-12-20 23:51:53 -0600

    lab02(reactive-web-with-spring-boot): add thymeleaf template

--- lab02-reactive-web-with-spring-boot/src/main/java/com/labs/reactive/springboot/Lab02Application.java
+++ lab02-reactive-web-with-spring-boot/src/main/java/com/labs/reactive/springboot/Lab02Application.java
@@ -2,6 +2,8 @@ package com.labs.reactive.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 public class Lab02Application {
@@ -10,4 +12,8 @@ public class Lab02Application {
     SpringApplication.run(Lab02Application.class, args);
   }
 
+  @Bean
+  public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+    return new HiddenHttpMethodFilter(); // need this bean to make the Http DELETE methods work properly
+  }
 }
